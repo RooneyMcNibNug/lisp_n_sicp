f6dbae3b860577eaacee1f9710c548d1fab609f1
JA <Jakea350@protonmail.com>
2025-01-10 20:53:37 +0000

    fix wget and such

--- install_scheme.sh
+++ install_scheme.sh
@@ -3,9 +3,6 @@
 ### Scripted instructions from https://www.gnu.org/software/mit-scheme/documentation/stable/mit-scheme-user/Unix-Installation.html
 ### Use this for a RPM-based system (I have this working on Fedora, for example)
 
-# MIT Scheme download base URL
-SCHEME_BASE_URL="https://ftp.gnu.org/gnu/mit-scheme/stable.pkg"
-
 # Ensure running as root for installation
 if [ "$EUID" -ne 0 ]; then
     echo "Please run this script as root!"
@@ -19,23 +16,23 @@ dnf install -y gcc make m4 ncurses-devel
 
 # Find the latest version of MIT Scheme
 echo "Finding latest MIT Scheme version..."
-LATEST_VERSION=$(wget -qO- "$SCHEME_BASE_URL" | grep -oP 'href="\K[^"]*' | grep -E '^[0-9]+\.[0-9]+' | sort -V | tail -n 1)
+LATEST_VERSION=$(wget -qO- "https://ftp.gnu.org/gnu/mit-scheme/stable.pkg/" | grep -oP 'href="\K[^"]*' | grep -E '^[0-9]+\.[0-9]+' | sort -V | tail -n 1)
 
 # Build URL based off of latest ver found
-SCHEME_TARBALL="$SCHEME_BASE_URL/$LATEST_VERSION/mit-scheme-${LATEST_VERSION}.tar.gz"
+SCHEME_TARBALL="https://ftp.gnu.org/gnu/mit-scheme/stable.pkg/${LATEST_VERSION}mit-scheme-${LATEST_VERSION}-x86-64.tar.gz
 
 # Download the source tar file
 if [ ! -f "mit-scheme-${LATEST_VERSION}.tar.gz" ]; then
     echo "Downloading MIT Scheme $LATEST_VERSION..."
-    wget "$SCHEME_TARBALL" -O "mit-scheme-${LATEST_VERSION}.tar.gz"
+    wget "$SCHEME_TARBALL" -O "mit-scheme.tar.gz"
 fi
 
 # Extract the tar
 echo "Extracting tarball..."
-tar -xzf "mit-scheme-${LATEST_VERSION}.tar.gz"
+tar -xzf "mit-scheme.tar.gz"
 
 # Change into the src dir and configure, build
-cd "mit-scheme-${LATEST_VERSION}/src"
+cd "mit-scheme/src"
 echo "Configuring and Building..."
 ./configure
 make
